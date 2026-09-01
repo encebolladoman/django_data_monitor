20204008a2d16af6b66a3121ec7b3710eaba1451
encebolladoman <92659471+encebolladoman@users.noreply.github.com>
2026-09-01 02:04:33 +0000

    clonacion

---
+++ dashboard/views.py
@@ -0,0 +1,22 @@
+from django.shortcuts import render
+import requests
+from django.conf import settings
+from django.contrib.auth.decorators import login_required, permission_required
+
+@login_required
+@permission_required('dashboard.index_viewer', raise_exception=True)
+def index(request):
+
+    response = requests.get(settings.API_URL)  # URL de la API
+    posts = response.json()  # Convertir la respuesta a JSON
+
+    # Número total de respuestas
+    total_responses = len(posts)
+
+
+    data = {
+        'title': "Landing Page' Dashboard",
+        'total_responses': total_responses,
+    }
+
+    return render(request, 'dashboard/index.html', data)
