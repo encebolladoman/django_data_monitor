6ff291fdb7786cdcb055b57c53219367ebcf3ac2
encebolladoman <92659471+encebolladoman@users.noreply.github.com>
2026-09-01 02:51:51 +0000

    correcion

--- backend_analytics_server/settings.py
+++ backend_analytics_server/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = "backend_analytics_server.wsgi.application"
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.environ.get('MYSQL_DATABASE'),
+        'NAME': os.environ.get('MYSQLDATABASE'),
         'USER': os.environ.get('MYSQLUSER'),
         'PASSWORD': os.environ.get('MYSQLPASSWORD'),
         'HOST': os.environ.get('MYSQLHOST'),
